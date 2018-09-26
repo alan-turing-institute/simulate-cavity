319c1fcaf4664288cbe25deec2f6e60b2a060686
masonlr <masonlr@unimelb.edu.au>
2018-09-26 22:01:39 +0100

    feat: run script to auto-detect qsub availability

--- simulate/run.sh
+++ simulate/run.sh
@@ -3,7 +3,19 @@
 SIMULATE="simulate"
 STATE="$SIMULATE/state"
 
-# save PBS_JOBID in state file via tee
-qsub -o log.qsub.stdout -e log.qsub.stderr pbs.sh | tee $STATE/pbs_job_id
 
-echo "Submitted job"
+QSUB=$(command -v qsub)
+
+if [ -n "$QSUB" ]; then
+    # save PBS_JOBID in state file via tee
+    echo "Submitted job"
+    qsub -o log.qsub.stdout -e log.qsub.stderr pbs.sh | tee $STATE/pbs_job_id
+else
+    echo "manual" > $STATE/pbs_job_id
+    python entry.py
+fi
+
+
+
+
+
